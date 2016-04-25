7046a723ca3cd4c938ad7e2dc8ddc3b3310ffef1
Frankenmint <Frankenmint@users.noreply.github.com>
2016-04-24 22:02:25 -0700

    Update loader.sh

--- loader.sh
+++ loader.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 
-if [ -z "$1" ]
+if [ -z "$1" ] || [ -z "$2" ] || [ -z "$3" ] || [ -z "$3" ]
 then
-	echo "Please Include a Filename"
-elif ! [ -z "$1" ]
+	echo "Some Arguments are Missing"
+elif ! [ -z "$1" ] || ![-z "$2" ] ||  ![ -z "$3" ] ||  ![ -z "$4" ]
 then
-	screen -dm bash -c "./escrowTEMPLATE.sh $1"
+	screen -dm bash -c "./escrow.shit $1 $2 $3 $4"
 	exit
 fi
 
